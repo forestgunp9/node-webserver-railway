f0da029fcd9e84cc4ec51c8e907c1b82bc81f65d
Diego Celery <forestgunp9@hotmail.com>
2023-08-11 16:28:54 -0400

    agrego .env a gitignore

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 //npm i express@4.17.1 
+
+//Al final teniendo todo, hacemos el siguiente despliegue de la app en la nube
 //git init
+//git add .    agrega todos los archivos del proyecto menos los gitignore
+//> git commit -m "Webserver listo"     tomamos la foto
+//Desplegar app en Railway.app
+//Hay muchas formas de hacerlo: Dockers, maquina virtual y desplegarlo, servicios de hosting para desplegar, a mano en un servidor local instalando Node, etc, etc
+//Firbase hosting, Amazon WebServices, Azure, DigitalOcean, Google Cloud, Heroku, Railway.
+//Railway no pide tarjeta de credito y se puede usar en produccion.
+//Creo repositorio en GitHub para luego subir el proyecto, llamado: node-webserver-railway
+//Subo mi proyecto a github al repsitorio creado:
+// git remote add origin https://github.com/forestgunp9/node-webserver-railway.git
+// git branch -M main       (renombra la rama actual a main en lugar de master, que es lo que se aconseja actualmente)
+// git push -u origin main  (Sube el proyecto a github al origen indicado , creado antes mas ariba)
+// Luego vamos a la pagina de Railway, creamos nuevo proyecto, y elegimos desplegar desde GitHub, buscandolo con el nombre node-webserver-railway.
+// Listo, proyecto github desplegado en Railway
+
+
 
 const express = require('express');
 const hbs = require('hbs');
